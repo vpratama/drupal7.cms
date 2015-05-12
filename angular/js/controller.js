2c6f39cad077cebaeac2a89c8196544ad2435a18
vitareinforce <vita.reinforce@gmail.com>
2015-05-12 15:19:32 +0700

    Remove unussed directory

--- angular/js/controller.js
+++
@@ -1,7 +0,0 @@
-var app = angular.module('myApp', []);
-app.controller('customersCtrl', 
-  function($scope, $http) {
-    $http.get("http://drupal7.cms/json/foods")
-    .success(function (response) {$scope.result = response.nodes;});
-  }
-);
