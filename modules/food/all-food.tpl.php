4cff2a60e7580c98aaa41c0f5e0c340e774aca94
vitareinforce <vita.reinforce@gmail.com>
2015-05-20 16:06:18 +0700

    update 20 Mei 2015 | AngularJS Form input and Filter Still on Research

--- modules/food/all-food.tpl.php
+++ modules/food/all-food.tpl.php
@@ -1,14 +1,14 @@
 <div id="content" role="main">
 	<div ng-app="myApp" ng-controller="customersCtrl">
 		<div class="input-append">
-          <input type="text" ng-model="search.node.title" autocomplete="false" style="width: 300px">
+          <input type="text" ng-model="search" autocomplete="false" style="width: 300px">
           <button type="submit" class="btn">Filter</button>
         </div>
 		
 		<a ng-click="hidden()">Click to Hide / Show using AngularJS ng-Hide</a>
 		<div ng-init="hideShow = false" class="custom-show-hide" ng-hide="hideShow"> 
 			<div ng-repeat="nodes in result"> 
-			  <div ng-animate="'animate'" ng-repeat="node in nodes | filter:search">
+			  <div ng-repeat="node in nodes | filter:search">
 			    <h2 class="node__title node-title">{{ node.title }}</h2>
 				<div ng-repeat="image in node">
 				    <img alt="{{ image.alt }}" src="{{ image.src }}" width="200px" />	
