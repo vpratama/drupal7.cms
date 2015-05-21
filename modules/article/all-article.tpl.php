53d302f01b1a86b6cc8b6e835096539a15938314
vitareinforce <vita.reinforce@gmail.com>
2015-05-21 13:10:52 +0700

    Update 21 Mei 2015 | add AngularJS Filtering

--- modules/article/all-article.tpl.php
+++ modules/article/all-article.tpl.php
@@ -1,14 +1,17 @@
 <div id="content" role="main">
 	<div ng-app="myApp" ng-controller="customersCtrl"> 
-		<div ng-repeat="nodes in result"> 
-		  <div ng-repeat="node in nodes">
-			
-		    <h2 class="node__title node-title">{{ node.title }}</h2>
-		    {{ node["Post date"]}}
+		<div class="input-append">
+          Filter : <input type="text" ng-model="search" autocomplete="false" style="width: 300px">
+        </div>
+		<a ng-click="hidden()">Click to Hide / Show using AngularJS ng-Hide</a>
+		<div ng-init="hideShow = false" class="custom-show-hide" ng-hide="hideShow">
+		  <div ng-repeat="nodes in result  | filter:search">
+			<h2 class="node__title node-title">{{ nodes.node.title }}</h2>
+		    {{ nodes.node["Post date"]}}
 		    <br>
 		    <br>
 		    <br>
-		    <p align="justify">{{ node.Body }}</p>
+		    <p align="justify">{{ nodes.node.Body }}</p>
 			<br />
 			<br />
 		  </div>
--- modules/article/js/controller.js
+++ modules/article/js/controller.js
@@ -1,6 +1,9 @@
 var app = angular.module('myApp', []);
 app.controller('customersCtrl', 
   function($scope, $http) {
+  	$scope.hidden = function() {
+        $scope.hideShow = !$scope.hideShow;
+    }
     $http.get("json/article")
     .success(function (response) {$scope.result = response.nodes;});
   }
