a3f71c122a1f7aba147ff8e0fa5f5a9c77fa00e4
vitareinforce <vita.reinforce@gmail.com>
2015-05-21 12:41:38 +0700

    Update 21 Mei 2015 | add AngularJS Filtering

--- modules/food/all-food.tpl.php
+++ modules/food/all-food.tpl.php
@@ -1,22 +1,23 @@
+
 <div id="content" role="main">
 	<div ng-app="myApp" ng-controller="customersCtrl">
 		<div class="input-append">
-          <input type="text" ng-model="search" autocomplete="false" style="width: 300px">
-          <button type="submit" class="btn">Filter</button>
+          Filter : <input type="text" ng-model="search" autocomplete="false" style="width: 300px">
         </div>
-		
 		<a ng-click="hidden()">Click to Hide / Show using AngularJS ng-Hide</a>
 		<div ng-init="hideShow = false" class="custom-show-hide" ng-hide="hideShow"> 
-			<div ng-repeat="nodes in result"> 
-			  <div ng-repeat="node in nodes | filter:search">
-			    <h2 class="node__title node-title">{{ node.title }}</h2>
-				<div ng-repeat="image in node">
-				    <img alt="{{ image.alt }}" src="{{ image.src }}" width="200px" />	
-				</div>
-				<b>Price :</b> {{ node.Price }}
-				<br />
-			  </div>
-			</div>
+			<div ng-repeat="nodes in result | filter:search">
+        <div>    
+          {{nodes.node.title}} 
+        </div>
+        <div>
+          <img alt="{{nodes.node.Image.alt}}" width="300px" src="{{nodes.node.Image.src}}" />
+        </div>
+        <div>
+          Price : {{nodes.node.Price}}
+        </div>
+        <br />
+      </div>
 		</div>
 	</div>
 </div>
